395adfa1ba663d3c0ba6f6d90f5ee2d3f324be56
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-15 10:20:13 +0200

    test(exo2-11-3.js): add test file

---
+++ Logicoperator2-11/exo2-11-3.js
@@ -0,0 +1,8 @@
+/*Quel est le résultat de AND ?
+importance: 5
+
+Qu’est-ce que ce code va afficher ?
+
+alert( 1 && null && 2 );*/
+
+alert( 1 && null && 2 ); // -->  null car première valeur fausse

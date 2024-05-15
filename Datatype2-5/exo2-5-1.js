5f94f16bdaae2ad082309e1d3dd856a2d53ec03e
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-15 09:59:53 +0200

    feat(exo2-5-1.js): add exo file

---
+++ Datatype2-5/exo2-5-1.js
@@ -0,0 +1,27 @@
+/**
+ * String quotes
+ * 
+ * importance: 5
+
+Quelle est la sortie du script ?
+
+let name = "Ilya";
+
+alert( `hello ${1}` ); // ?
+
+alert( `hello ${"name"}` ); // ?
+
+alert( `hello ${name}` ); // ?
+ */
+
+/**
+ * 
+ * 
+let name = "Ilya";
+
+alert(`hello ${1}`); --> "hello 1"
+
+alert(`hello ${"name"}`);  --> "hello name"
+
+alert(`hello ${name}`); --> "hello Ilya"
+ */

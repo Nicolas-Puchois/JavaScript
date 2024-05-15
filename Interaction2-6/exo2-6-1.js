4acd162902dca41182fa105aefa54e1a5198852a
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-15 09:59:48 +0200

    feat(exo2-6-1.js): add exo file

---
+++ Interaction2-6/exo2-6-1.js
@@ -0,0 +1,12 @@
+/**
+ * Une simple page 
+ * 
+ * 
+ * Créez une page Web qui demande un nom et l’affiche
+ */
+
+'use strict';
+
+let name = prompt("Write your name please","John");
+
+console.log(name);

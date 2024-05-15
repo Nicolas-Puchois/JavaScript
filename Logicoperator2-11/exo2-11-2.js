623847f74f47ff031d8fa2bafa20f906b8c56ee0
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-15 15:51:33 +0200

    chore(): remove deprecated file

--- Logicoperator2-11/exo2-11-2.js
+++
@@ -1,8 +0,0 @@
-/* Quel est le résultat des alertes OR ?
-importance: 3
-
-Qu’est-ce que le code ci-dessous va sortir ?
-
-alert( alert(1) || 2 || alert(3) );*/
-
-alert( alert(1) || 2 || alert(3));// --> pop-up avec écris "1" puis 2 car exécution de l'alert 1 puis 2 car première valeur vrai
--- Logicoperator2-11/exo2-11-3.js
+++
@@ -1,8 +0,0 @@
-/*Quel est le résultat de AND ?
-importance: 5
-
-Qu’est-ce que ce code va afficher ?
-
-alert( 1 && null && 2 );*/
-
-alert( 1 && null && 2 ); // -->  null car première valeur fausse

6cbb47a31bb1084219633b06a59a92ef4a5be876
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-15 10:20:04 +0200

    test(exo2-11-1.js): add test file

---
+++ Logicoperator2-11/exo2-11-1.js
@@ -0,0 +1,9 @@
+/*Quel est le résultat de OR ?
+importance: 5
+
+Qu’est-ce que le code ci-dessous va sortir ?
+
+alert( null || 2 || undefined );*/
+
+
+alert( null || 2 || undifined); //--> 2 car premier valeur vrai

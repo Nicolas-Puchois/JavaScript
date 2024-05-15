f5713ed3491e606e8ea7bf4eb3c9d3feaf334b02
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-15 10:08:36 +0200

    feat(exo2-10-1.js): add exo file

---
+++ Ifinstruction2-10/exo2-10-1.js
@@ -0,0 +1,11 @@
+/*if (une chaîne de caractères avec zéro)
+
+importance: 5
+
+Est-ce que alert sera affiché ?
+
+if ("0") {
+  alert( 'Hello' );
+}*/
+
+// --> Hello sera afficher car le string n'est pas vide

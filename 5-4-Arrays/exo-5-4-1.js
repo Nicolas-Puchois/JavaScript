1e5e2b65a9ba8b9c43b8ce120ff384cee38f71fb
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-16 18:52:18 +0200

    feat(exo5-4-1.js): add exo file

---
+++ 5-4-Arrays/exo-5-4-1.js
@@ -0,0 +1,20 @@
+/**
+ * Le tableau est-il copié ?
+importance: 3
+
+Qu’est-ce que ce code va montrer ?
+
+let fruits = ["Apples", "Pear", "Orange"];
+
+// pousser une nouvelle valeur dans la "copie"
+let shoppingCart = fruits;
+shoppingCart.push("Banana");
+
+// Qu'y a-t-il dans fruits ?
+alert( fruits.length ); // ?
+ */
+
+
+/**
+ * ca va renvoyer 4 car ShoppingCart et Fruits sont les même tableau avec les même propriété
+ */

cf206227cc476bda1274f74b6866b17e9e4c7498
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-16 18:51:37 +0200

    feat(exo4-1-1.js): add exo file

---
+++ 4-1-Object/exo4-1-1.js
@@ -0,0 +1,27 @@
+/**
+ * Bonjour objet
+importance: 5
+
+Écrivez le code, une ligne pour chaque action :
+
+    Créer un objet vide user.
+    Ajoutez la propriété name avec la valeur John.
+    Ajoutez la propriété surname avec la valeur Smith.
+    Changer la valeur de name pour Pete.
+    Supprimez la propriété name de l’objet.
+
+
+ * 
+ */
+
+'use strict';
+
+let user=  {}
+
+user.name = 'John';
+
+user.surname = 'Smith';
+
+user.name = 'Pete';
+
+delete user.name;

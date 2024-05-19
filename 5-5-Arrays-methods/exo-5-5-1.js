4fedadf33b6f682554a026cec9ce6bc02385c834
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-19 21:56:39 +0200

    feat(exo-5-5-1.js): add exo file

---
+++ 5-5-Arrays-methods/exo-5-5-1.js
@@ -0,0 +1,19 @@
+/*
+Traduit border-left-width en borderLeftWidth
+importance: 5
+
+Ecrivez la fonction camelize(str) qui change les mots séparés par des tirets comme “my-short-string” en camel-cased “myShortString”.
+
+La fonction doit donc supprimer tous les tirets et mettre en majuscule la première lettre de chaque mot à partir du deuxième mot.
+
+Exemples :
+
+camelize("background-color") == 'backgroundColor';
+camelize("list-style-image") == 'listStyleImage';
+camelize("-webkit-transition") == 'WebkitTransition';
+
+P.S. Astuce : utilisez split pour scinder la chaîne dans un tableau, transformer la et ensuite utilisez join.
+*/
+
+
+let camelize

f59a6a4653e0ce1dfd93cdbae978c50dde53de86
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-19 21:58:32 +0200

    feat(exo-5-5-2.js): add exo file

---
+++ 5-5-Arrays-methods/exo-5-5-2.js
@@ -0,0 +1,18 @@
+/* 
+Filter range
+importance: 4
+
+Ecrivez une fonction filterRange(arr, a, b) qui obtient un tableau arr, recherche les éléments avec des valeurs supérieures ou égales à a et inférieures ou égales à b et retourne un résultat sous forme de tableau.
+
+La fonction ne doit pas modifier le tableau. Elle doit juste retourner le nouveau tableau.
+
+Par exemple :
+
+let arr = [5, 3, 8, 1];
+
+let filtered = filterRange(arr, 1, 4);
+
+alert( filtered ); // 3,1 (valeurs correspondantes)
+
+alert( arr ); // 5,3,8,1 (non modifié)
+*/

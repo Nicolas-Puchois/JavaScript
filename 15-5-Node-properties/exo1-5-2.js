4ff2969f8261ec4ed7535f51318c6d35672ee91d
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-28 10:27:06 +0200

    feat(exo-1-5-2.js): add exo file

---
+++ 15-5-Node-properties/exo1-5-2.js
@@ -0,0 +1,18 @@
+/*
+What's in the nodeType?
+importance: 5
+
+What does the script show?
+
+<html>
+
+<body>
+  <script>
+    alert(document.body.lastChild.nodeType);
+  </script>
+</body>
+
+</html>
+*/
+
+// its show 1 because its show the 'script' tags which noteType = 1

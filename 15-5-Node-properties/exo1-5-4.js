91e359c73ad6f5a91763256c608a6b862de02262
Nicolas-Puchois <nicolas.puchois@laposte.net>
2024-05-28 10:27:13 +0200

    feat(exo-1-5-4.js): add exo file

---
+++ 15-5-Node-properties/exo1-5-4.js
@@ -0,0 +1,13 @@
+/*
+Where's the "document" in the hierarchy?
+importance: 4
+
+Which class does the document belong to? its class is HTMLDocument
+
+What’s its place in the DOM hierarchy?
+its place between HTMLDocument and  
+
+Does it inherit from Node or Element, or maybe HTMLElement?
+he inherit from HTMLDocument
+*/
+
